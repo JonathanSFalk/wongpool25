ea6f1532dae40e7fb384f740cb567f69695e27e5
jonathan.falk@marginalutilityllc.com <jonathan.falk@marginalutilityllc.com>
2024-02-24 17:32:07 +0000

    Edited Module1

--- client_code/Module1.py
+++ client_code/Module1.py
@@ -50,9 +50,10 @@ def get_all_homers(date):
 custom_signup.login_flow.do_email_confirm_or_reset()
 # Open Form1
 open_form('signup')
-while user is Npne:
-  user = anvil.users.get_user()
-open_form('TeamPicker')
+user = anvil.users.get_user()
+#while user is None:
+#  user = anvil.users.get_user()
+#open_form('TeamPicker')
 #update_text = update()
 #z=anvil.server.call('check')
 #tlist = anvil.server.call('team_list')
