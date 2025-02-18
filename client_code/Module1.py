ac3f1d870edb9c35ec7820c4de69a40051a1577b
jonathan.falk@marginalutilityllc.com <jonathan.falk@marginalutilityllc.com>
2025-02-18 17:28:29 +0000

    Edited Module1 and signup

--- client_code/Module1.py
+++ client_code/Module1.py
@@ -46,10 +46,10 @@ def get_all_homers(date):
                         home = thegame['teams']['home']['team']['id']
                         retn.append([ldict[l][0],date,gn,home,homers,dh])
     return retn
-open_form('SplashScreen')
+#open_form('SplashScreen')
 #custom_signup.login_flow.do_email_confirm_or_reset()
 # Open Form1
-#open_form('signup')
+open_form('signup')
 #user = anvil.users.get_user()
 #while user is None:
 #  user = anvil.users.get_user()
